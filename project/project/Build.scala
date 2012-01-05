b076496da15cfede4fece7780cb9cd56305d4d52
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-01-05 13:27:10 +0100

    add scalariform

--- project/project/Build.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-object PluginDef extends Build {
-    override lazy val projects = Seq(root)
-    lazy val root = Project("plugins", file(".")) dependsOn( webPlugin )
-    lazy val webPlugin = uri("git://github.com/typesafehub/sbt-scalariform")
-}
-
-// vim: set ts=2 sw=2 et:
