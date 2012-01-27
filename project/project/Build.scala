1872d5da2c7b1df4bc233ed31b90a1c34db15852
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-01-27 21:21:33 +0100

    updates scalariform plugin

--- project/project/Build.scala
+++ project/project/Build.scala
@@ -2,7 +2,7 @@ import sbt._
 object PluginDef extends Build {
     override lazy val projects = Seq(root)
     lazy val root = Project("plugins", file(".")) dependsOn( webPlugin )
-    lazy val webPlugin = uri("git://github.com/typesafehub/sbt-scalariform")
+    lazy val webPlugin = uri("git://github.com/typesafehub/sbtscalariform.git")
 }
 
 // vim: set ts=2 sw=2 et:
