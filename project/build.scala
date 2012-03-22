ff353c78af722535753e42bcf58335d50c5a399c
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-03-22 13:32:44 +0100

    fix build definition

--- project/build.scala
+++ project/build.scala
@@ -1,11 +1,12 @@
 import sbt._
+import Keys._ 
 
 object ScalaInflectorBuild extends Build {
 
 
   val root = (Project("scala-inflector", file("."))
               settings(externalResolvers <<= resolvers map { rs => 
-                Resolver.withDefaultResolvers(rs, mavenCentral = true, scalaTools = false) 
-              })
+                Resolver.withDefaultResolvers(rs, mavenCentral = true, scalaTools = false)
+              }))
 }
 
