e67ca8b10e8a7f50a8fd0c6825210d457c4f7de7
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-03-22 12:42:48 +0100

    disable scala-tools

--- project/build.scala
+++ project/build.scala
@@ -3,6 +3,9 @@ import sbt._
 object ScalaInflectorBuild extends Build {
 
 
-  val root = Project("scala-inflector", file("."))
+  val root = (Project("scala-inflector", file("."))
+              settings(externalResolvers <<= resolvers map { rs => 
+                Resolver.withDefaultResolvers(rs, mavenCentral = true, scalaTools = false) 
+              })
 }
 
